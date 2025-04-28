7ea751e2ef54f3084a65bfa8638fabd9e5e872f7
Himanshu Ahuja <himanshu@nicolethemathlady.com>
2025-04-28 12:10:47 -0400

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  generateBuildId: async () => {
+    return Date.now().toString();
+  },
+  env: {
+    NEXT_PUBLIC_BUILD_ID: process.env.BUILD_ID || Date.now().toString(),
+  },
 };
 
 export default nextConfig;
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
